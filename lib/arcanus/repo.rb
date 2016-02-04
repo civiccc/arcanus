civiccc/arcanus

commit 15e3bd01e952fe18c2fe2d09d190bef361ee560f

Remove need for .git directory to exist
When deploying with Capistrano, repos won't have the `.git` directory.
Thus we need to change the logic to not require its existence.

lib/arcanus/repo.rb
@@ -12,44 +12,22 @@ module Arcanus
     # current working directory resides within.
     #
     # @return [String]
-    # @raise [Arcanus::Errors::InvalidGitRepoError] if the current directory
+    # @raise [Arcanus::Errors::InvalidArcanusRepoError] if the current directory
     #   doesn't reside within a git repository
     def root
       @root ||=
         begin
-          git_dir = Pathname.new(File.expand_path('.'))
+          arc_dir = Pathname.new(File.expand_path('.'))
                             .enum_for(:ascend)
                             .find do |path|
-            (path + '.git').exist?
+            (path + '.arcanus').exist?
           end
 
-          unless git_dir
-            raise Errors::InvalidGitRepoError, 'no .git directory found'
+          unless arc_dir
+            raise Errors::InvalidArcanusRepoError, 'no .arcanus directory found'
           end
 
-          git_dir.to_s
-        end
-    end
-
-    # Returns an absolute path to the .git directory for a repo.
-    #
-    # @return [String]
-    def git_dir
-      @git_dir ||=
-        begin
-          git_dir = File.expand_path('.git', root)
-
-          # .git could also be a file that contains the location of the git directory
-          unless File.directory?(git_dir)
-            git_dir = File.read(git_dir)[/^gitdir: (.*)$/, 1]
-
-            # Resolve relative paths
-            unless git_dir.start_with?('/')
-              git_dir = File.expand_path(git_dir, repo_dir)
-            end
-          end
-
-          git_dir
+          arc_dir.to_s
         end
     end
 

lib/arcanus/errors.rb
@@ -27,8 +27,9 @@ module Arcanus::Errors
   # Raised when invalid/non-existent command was used.
   class CommandInvalidError < UsageError; end
 
-  # Raised when run in a directory not part of a valid git repository.
-  class InvalidGitRepoError < UsageError; end
+  # Raised when run in a directory not part of a repository with Arcanus
+  # installed.
+  class InvalidArcanusRepoError < UsageError; end
 
   # Raised when a key path corresponding to a non-existent key is specified.
   class InvalidKeyPathError < UsageError; end

commit 0eae6acb116fe4425df80bf1df353cefedcb1c60

Fix arcanus setup to work on new repo
This was a regression introduced by 15e3bd0.

lib/arcanus/repo.rb
@@ -20,7 +20,10 @@ module Arcanus
           arc_dir = Pathname.new(File.expand_path('.'))
                             .enum_for(:ascend)
                             .find do |path|
-            (path + '.arcanus').exist?
+            # We check for .arcanus first since most repos will have that but
+            # not necessarily have .git. However, when running `arcanus setup`,
+            # the .arcanus directory won't exist yet, so check for .git
+            (path + '.arcanus').exist? || (path + '.git').exist?
           end
 
           unless arc_dir